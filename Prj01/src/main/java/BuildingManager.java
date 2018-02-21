b58b0d8d2a8ef19a43d614c39971d3e3561c5aa4
Maria <mari.tolstogyzova@gmail.com>
2018-02-21 19:55:39 +0300

    Modified output

--- Prj01/src/main/java/BuildingManager.java
+++ Prj01/src/main/java/BuildingManager.java
@@ -10,10 +10,10 @@ public class BuildingManager {
         building.addRoom("Room 1", 50, 2);
         building.addRoom("Room 2", 80, 4);
         building.getRoom("Room 1").add(new Lamp(150));
-        building.getRoom("Room 1").add(new Chair("So soft that you can drown in me", 1, 2));
+        building.getRoom("Room 1").add(new Chair("Chair \"So soft that you can drown in me\"", 1, 2));
         building.getRoom("Room 2").add(new Lamp(550));
-        building.getRoom("Room 2").add(new Sofa("Lie on me gentle", 3, 4));
-        building.getRoom("Room 2").add(new Table("Don't seat over here", 6));
+        building.getRoom("Room 2").add(new Sofa("Sofa \"Lie on me gentle\"", 3, 4));
+        building.getRoom("Room 2").add(new Table("Table \"Don't seat over here\"", 6));
         building.describe();
     }
 }
